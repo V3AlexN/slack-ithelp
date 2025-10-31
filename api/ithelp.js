12e7b72de62168850269cae92955ca7351993a52
IT Admin <itadmin@ITs-Mac-mini.local>
2025-10-31 10:42:07 -0700

    Initial commit

---
+++ api/ithelp.js
@@ -0,0 +1,7 @@
+export default async function handler(req, res) {
+  const helpCenterURL = "https://v3electric.atlassian.net/servicedesk/customer/portals";
+  res.status(200).json({
+    response_type: "ephemeral",
+    text: `Need IT support? Submit a ticket here 👉 <${helpCenterURL}>`
+  });
+}
